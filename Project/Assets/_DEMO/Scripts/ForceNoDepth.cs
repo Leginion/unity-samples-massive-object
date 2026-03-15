e9faf90ed60ab2bfc251eab7211a2a3a85afceaf
LazyKnight <i@lazyknight.com>
2026-03-16 03:19:21 +0800

    perf: 100k-1 移除Camera Depth & 光照不渲染shadow ，提升编辑器FPS 4->5 运行时FPS 10->13

---
+++ Project/Assets/_DEMO/Scripts/ForceNoDepth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ForceNoDepth : MonoBehaviour
+{
+    void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        if (cam != null)
+        {
+            cam.depthTextureMode = DepthTextureMode.None;
+            Debug.Log("Forced depthTextureMode = None");
+        }
+    }
+}
